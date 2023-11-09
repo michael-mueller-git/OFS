452bcf4ab4eb3b89e2c9a96396905fd2f4afb685
nix <nix@local>
2023-11-09 19:01:36 +0100

    compiles but ERROR: wayland not availabl

--- flake.nix
+++ flake.nix
@@ -12,32 +12,34 @@
     in
     {
       packages.x86_64-linux.ofs = pkgs.stdenv.mkDerivation {
-        pname = "OFS";
+        pname = "OpenFunscripter";
         version = "4.0.0";
         src = pkgs.fetchgit {
           url = "https://github.com/michael-mueller-git/OFS.git";
-          rev = "42a9f1a07941dda70a1c6c58c3be0612fd44129d";
-          sha256 = "sha256-UFU3Ln/XdjjujpPSrnIK0EPFw+/ZJ10fBLSe4ZA3PH4=";
+          rev = "a95b3a68be691ca6d2dae1f7ae51988227fafe87";
+          sha256 = "sha256-B8efNEukT/cakNmNMCO5PDP+A0GmNvxCPWOSRwGFgXE=";
         };
-        cmakeFlags = [ "-Wno-dev" "--compile-no-warning-as-error" "-DCFLAGS=-Wno-error" ];
+        cmakeFlags = [ "-Wno-dev" "--compile-no-warning-as-error" "-DCFLAGS=-Wno-error" "-DCXXFLAGS=-Wno-error" ];
         nativeBuildInputs = with pkgs; [
           cmake
           mesa
           libGLU
+          libglvnd
           pkg-config
           mpv
         ];
       };
       defaultPackage.x86_64-linux = self.packages.x86_64-linux.ofs;
       formatter.${system} = pkgs.nixpkgs-fmt;
-       devShells.x86_64-linux.default = pkgs.mkShell {
+      devShells.x86_64-linux.default = pkgs.mkShell {
         shellHook = ''
-          echo " >> Welcome to NixOS developer shell"
+          echo " >> Welcome to Nix developer shell"
         '';
         nativeBuildInputs = with pkgs; [
           cmake
           mesa
           libGLU
+          libglvnd
           pkg-config
           mpv
         ];
